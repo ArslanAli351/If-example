8c9eb9d67ba3a28849d4838ca90091bedf63c0e2
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-30 19:15:59 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+let amprature=prompt("type your City Name")
+let tamprature=+prompt("type tamprature")
+if(amprature==="karachi" && tamprature<=20){
+    document.write("tamprature is cool")
+}
+else if(tamprature>=21){
+    document.write("amprature is Hoot")
+           }
